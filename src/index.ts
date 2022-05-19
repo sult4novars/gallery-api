e077b10a5db54ef9c06905d1695b72cd44666722
Arsen <sultanov.arsen@luckymonkeys.games>
2022-05-20 00:45:07 +0300

    add ping

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import router from './controllers';
+// import mongoose from 'mongoose';
+// import router from './controllers';
+import type { Request, Response } from 'express';
 
 const app = express();
 
-async function dbConnect() {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/gallery');
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function dbConnect() {
+//   try {
+//     await mongoose.connect('mongodb://localhost:27017/gallery');
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 function onServerStart() {
   return console.log('http://localhost:9090/');
 }
 
-dbConnect();
+// dbConnect();
 
-app.use(express.json()).use('/api/v1', router).listen(9090, onServerStart);
+const ping = (req: Request, res: Response) => res.status(200).send('pong');
+
+app.use(express.json()).use('/ping', ping).listen(9090, onServerStart);
